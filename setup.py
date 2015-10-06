2797e2e5ed78fcf28342675bfd29a36f7ad8a0dd
Necdet Can Atesman <c.atesman@gmx.net>
2015-10-06 15:13:29 +0200

    Added license and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.es',
-    version='0.1.7'
+    version='0.1.7',
     description='ElasticSearch integration for The SCORE Framework',
     long_description=README,
     author='strg.at',
@@ -15,6 +15,22 @@ setup(
     url='http://score-framework.org',
     keywords='score framework elasticsearch',
     packages=['score.es'],
+    license='LGPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General '
+            'Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
     install_requires=[
         'elasticsearch >= 1.0, < 2.0',
     ]
